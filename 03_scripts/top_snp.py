1cfd98ebdfb59488581d53d98e1d031202edac76
Thomas Ellis <thomas.ellis@gmi.oeaw.ac.at>
2022-02-04 10:09:14 +0100

    Fixed top_snp.py
    Fixed 03_scripts/top_snp.py so it outputs to 01_data, and removed the duplicate script in 04_analyses/03_condition_on_top_snp.
    
     Changes to be committed:
    	deleted:    01_data/chr2_5923326.csv
    	new file:   01_data/replicate_experiment.csv
    	renamed:    04_analyses/03_condition_on_top_snp/top_snp.py -> 03_scripts/top_snp.py
    	modified:   04_analyses/03_condition_on_top_snp/GWA_condition_on_top_SNP.sh

--- 03_scripts/top_snp.py
+++ 03_scripts/top_snp.py
@@ -1,22 +1,15 @@
 """
 Tom Ellis, 31st January 2022
 
-Script to pull out genotypes for top associations from the SNP matrix. The
-SNPs to be pulled are taken from a data file created by the R script
-`top_gwas_results.R`, so run that first.
+Script to pull out genotypes for the SNP most strongly associated with necrosis
+(Chr2:5927469).
 """
 
 import pandas as pd
 import numpy as np
 import h5py
-import os
 
 from snp_2_hdf5 import snp_2_hdf5
-
-# Get the directory of this script
-script = os.path.dirname(__file__)
-# Create output folder if this does not exist
-os.makedirs(script + "/phenotypes", exist_ok = True)
 
 # Import the 10-million-SNP matrix as an HDF5 file
 genoFile = '01_data/1001_SNP_MATRIX/all_chromosomes_binary.hdf5'
@@ -35,5 +28,5 @@ code = [ x.decode(encoding = 'UTF-8') for x in  geno_hdf['accessions'][()] ]
 snp.insert(0, 'code', code )
 
 # write to disk
-path = script + "/phenotypes/top_snp.txt"
+path = "01_data/top_snp.csv"
 snp.to_csv(path, header=True, index= False)
